1f3b1bb5da0fc58b9b5d44d883d20b122109b9eb
Sanches <sanches251193@yandex.ru>
2022-11-17 16:02:14 +0300

    if_else

---
+++ If_else.cpp
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+main(){
+	int a , b;
+	printf("Vvedite A and B\n");
+	scanf("%i", &a);
+	scanf("%i", &b);
+	if(a>b)
+	{
+		printf("%i bolshe",a);
+	}
+	else 
+	{
+		if(a<b)
+		{
+			printf("%i bolshe",b);
+		}
+		else
+		{
+			printf("%i = %i", a, b);
+		}
+	}
+	
+
+}
