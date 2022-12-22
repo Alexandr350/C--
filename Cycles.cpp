c234e88a3946c1e25ecfaf7b6ece3c41c0f87f6f
Sanches <sanches251193@yandex.ru>
2022-12-22 19:51:37 +0300

    Cycles

---
+++ Cycles.cpp
@@ -0,0 +1,44 @@
+#include <stdio.h>
+
+main(){
+	
+	int i = 0;
+	
+		while(i < 15){
+		printf("Iteration = %i, values = %i \n", i+1, i );
+		i++;
+	}
+	
+	printf("\n\n");
+	
+		while(i < 24){
+		printf("%i = Hello ", i-14);
+		i++;
+	}
+	
+	printf("\n\n");
+	printf("Iteration = %i\n", i);
+
+	
+	printf("\n\n");
+	
+	while(i>0){
+		if(i%2 != 0){
+			printf("   %i\n",i);
+		}
+		i--;
+		
+	}
+	
+	printf("\n\n");
+
+	int j = 5;
+	while(i <16){
+		
+		if(i > 5){
+			printf("Summa:  %i  +  %i = %i\n",j,i,j+i);
+			j = j+i;
+		}
+		i++;
+	}
+}
