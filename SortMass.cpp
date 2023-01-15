080ef017e019d9e5988d17a38423f904dfa04668
Sanches <sanches251193@yandex.ru>
2023-01-15 12:07:04 +0300

    SortMass

--- SortMass.cpp
+++ SortMass.cpp
@@ -6,31 +6,20 @@
 
 
 int main(){
-	setlocale(LC_ALL, "Russian");
-	//system("chcp 1251");
+	
 	printf("\nEnter size array ");
 	int *massiv;
 	int user_vvod = 0;
 	
-	while(true){
-		scanf("%i", &user_vvod);
+	scanf("%i", &user_vvod);
 		
-		if(user_vvod <= 100){
-			massiv = (int*)malloc(user_vvod * sizeof(int));
-			for(int i = 0; i < user_vvod; i++){
-				printf("Value  %i = ", i);
-				scanf("%i", &massiv[i]);
-			}
-			break;
-		}
 		
-		else{
-			printf("\nArray must not exceed 100 elements \n\nEnter size array ");
+	massiv = (int*)malloc(user_vvod * sizeof(int));
+		for(int i = 0; i < user_vvod; i++){
+			printf("Value  %i = ", i);
+			scanf("%i", &massiv[i]);
 		}
-	}
 	
-
-
 	int max = 0;
 	for(int i = 0; i < user_vvod; i++){
 		for(int j = 0; j < user_vvod-1; j++){
@@ -42,9 +31,11 @@ int main(){
 		}
 	}
 	
-	
+	int summa = 0;
 	for(int i = 0; i < user_vvod; i++){
-		printf("%i ", massiv[i]);
+		summa = summa + massiv[i];
 	}
+	int medium = summa/user_vvod;
+	printf("\nMin: %i \nMax: %i \nSumma: %i \nMedium: %i", massiv[0], massiv[user_vvod-1], summa, medium);
 	free(massiv);
 }
